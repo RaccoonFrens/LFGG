6cfe47373c20d9a424754be87db9b64c6dec6277
P2fryang <fryang@ucsd.edu>
2021-03-02 13:12:05 -0800

    user's posts on profile page

--- frontend/LFG/app/src/main/java/com/example/lfg/fragments/ProfileFragment.java
+++ frontend/LFG/app/src/main/java/com/example/lfg/fragments/ProfileFragment.java
@@ -6,19 +6,47 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.lfg.LoginActivity;
 import com.example.lfg.R;
+import com.example.lfg.adapters.PostsAdapter;
+import com.example.lfg.interfaces.ItemClickListener;
+import com.example.lfg.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
     private Button btnLogout;
+    private TextView tvUsername;
+    private TextView tvUserDetails;
+    private RecyclerView rvUserPosts;
+    private PostsAdapter postsAdapter;
+    private FirebaseDatabase database;
+    List<Post> posts;
+    private FirebaseAuth mAuth;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -35,7 +63,22 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        rvUserPosts = view.findViewById(R.id.rvUserPosts);
+        posts = new ArrayList<>();
         btnLogout = view.findViewById(R.id.btnLogout);
+
+        ItemClickListener itemClickListener = new ItemClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                final FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                Post post = posts.get(position);
+                Fragment fragment = new PostFragment(post);
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).addToBackStack("profile").commit();
+                //fragmentManager.beginTransaction().replace(R.id.,fragment).addToBackStack("profile").commit();
+
+                //TODO: show post details
+            }
+        };
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +89,86 @@ public class ProfileFragment extends Fragment {
                 getActivity().finish();
             }
         });
+
+        postsAdapter = new PostsAdapter(getContext(), posts, itemClickListener);
+        rvUserPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvUserPosts.setAdapter(postsAdapter);
+        database = FirebaseDatabase.getInstance();
+        loadData();
+    }
+    private void loadData(){
+        database.goOnline();
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        Log.i(TAG, mUser.getUid());
+
+        DatabaseReference ref = database.getReference("posts");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    if(!child.child("user").getValue().toString().equals(mUser.getUid())){
+                        continue;
+                    }
+                    //User tempUser = new User();
+                    Log.i("get", child.getKey());
+               /*     tempUser.setId((String) child.child("author").child("id").getValue());
+                    tempUser.setUsername((String) child.child("author").child("username").getValue());
+                    tempUser.setEmail((String) child.child("author").child("email").getValue());
+                    //tempUser.setEmail(child.child("author").child("posts").getValue());
+                    tempPost.setId((String) child.child("id").getValue());
+                    tempPost.setAuthor(tempUser);
+                    tempPost.setBody((String) child.child("body").getValue());
+                    tempPost.setTag((String) child.child("tag").getValue());
+                    tempPost.setDuration((Date) child.child("duration").getValue());
+                    tempPost.setCreatedAt((Date) child.child("createdAt").getValue());
+                    tempPost.setSize((int) ((long) child.child("size").getValue()));
+                    tempPost.setLogoName("minecraft_logo.png");
+                    //tempPost.setReplies(child.child("replies").getValue());
+                    //thePosts.add(tempPost);*/
+                    String game = (String) child.child("game").getValue();
+                    int size = (int) ((long) child.child("size").getValue());
+                    long time = (long) child.child("timestamp").getValue();
+                    long timer = (long) child.child("timer").getValue();
+                    String logo = (String) child.child("logoName").getValue();
+                    String authorId = (String) child.child("user").getValue();
+                    Post currPost = new Post(game, size, logo, time+timer);
+                    Date postTimestamp = new Date(time+timer);
+                    Log.i("TIME", String.valueOf(time));
+                    long currentTimestamp = System.currentTimeMillis();
+                    Date currentTime = new Date(currentTimestamp);
+                    if(postTimestamp.before(currentTime)){
+                        String postId = child.getKey();
+                        ref.child(postId).removeValue();
+                        database.getReference("users").child(authorId).child("posts").child(postId).removeValue();
+                        Log.i("expired", "timestamp: " + postTimestamp.toString());
+                        Log.i("expired", "current time: " + currentTime.toString());
+                        //posts.add(currPost);
+                    }
+                    else{
+                        Log.i("active", game + " timestamp: " + postTimestamp.toString());
+                        Log.i("active", game + " current time: " + currentTime.toString());
+                        posts.add(currPost);
+                    }
+                    //currPost.setLogoName("fortnite_logo.png");
+
+                }
+
+                Collections.sort(posts, new ProfileFragment.Sortbytime());
+                postsAdapter.notifyDataSetChanged();
+            }
+            @Override public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+    class Sortbytime implements Comparator<Post> {
+
+        @Override
+        public int compare(Post a, Post b) {
+            return (int) (a.getTimeEnd() - b.getTimeEnd());
+        }
     }
 }
