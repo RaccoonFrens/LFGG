29644c9179dd00fa7ca040ac778d6cb3de75219a
victormmontano <victormontano172@gmail.com>
2021-03-13 16:35:29 -0800

    filled parties sent to end of list

--- frontend/LFG/app/src/main/java/com/example/lfg/adapters/PostsAdapter.java
+++ frontend/LFG/app/src/main/java/com/example/lfg/adapters/PostsAdapter.java
@@ -121,7 +121,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
                 });
             } catch (IOException e ) {}
 
-            playerCount.setText(post.getSize() + " players");
+            playerCount.setText(post.getPlayers() + "/" + post.getSize() + " players");
             tag1.setText(post.getTag());
             if(tag1.getText().toString().equals("Casual")){
                 tag1.setBackgroundColor(Color.parseColor("#4E88E6"));
