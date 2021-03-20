650909193d7e29ca70cb83bb6e77b56ba0e9c3ae
Josh Chou <j1chou@ucsd.edu>
2021-03-20 04:46:22 -0700

    back to executive decision

--- frontend/LFG/app/src/main/java/com/example/lfg/fragments/PostFragment.java
+++ frontend/LFG/app/src/main/java/com/example/lfg/fragments/PostFragment.java
@@ -498,10 +498,12 @@ public class PostFragment extends Fragment {
                                     //can also be used to store other things
                                     try {
                                         matchTime = match.getString("gameStartTime");
-                                        tvMatch.setText("Match time:" + (System.currentTimeMillis() - Integer.parseInt(matchTime) / 60000) + " minutes");
+                                        tvMatch.setText("Currently in Match!");
+                                        //tvMatch.setText("Match time:" + (System.currentTimeMillis() - Integer.parseInt(matchTime) / 60000) + " minutes");
                                     } catch (JSONException e) {
                                         matchTime = "0";
-                                        tvMatch.setText("Match time:" + (System.currentTimeMillis() - Integer.parseInt(matchTime) / 60000) + " minutes");
+                                        tvMatch.setText("Currently in Match!");
+                                        //tvMatch.setText("Match time:" + (System.currentTimeMillis() - Integer.parseInt(matchTime) / 60000) + " minutes");
                                         e.printStackTrace();
                                     }
                                     Log.d("PostFragment", "Match in progress since " + (System.currentTimeMillis() - Integer.parseInt(matchTime) / 60000));
